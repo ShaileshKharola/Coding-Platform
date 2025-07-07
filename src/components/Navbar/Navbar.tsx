e8ee7747360aff7244fac4cd3ba202062cc03683
hunter <shaileshkharola0048@gmail.com>
2025-07-07 22:33:32 +0530

    kick start withnext js

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+type NavbarProps = {};
+const Navbar: React.FC<NavbarProps> = () => {
+    return <div>Na</div>;
+};
+export default Navbar;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </>
+  );
+}
